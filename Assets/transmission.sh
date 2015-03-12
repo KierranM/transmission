ee2edf390ea1be5a91c47b995234e82bd91cef8b
Kierran McPherson <kierran.mcpherson@xero.com>
2015-03-13 08:23:50 +1300

    Running transmission as root

--- Assets/transmission.sh
+++ Assets/transmission.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 umask 000
 
-exec /sbin/setuser nobody transmission-daemon --foreground --allowed 127.*,192.168.20.* --log-info
+exec /sbin/setuser root transmission-daemon --foreground --allowed 127.*,192.168.20.* --log-info
