e66a5a6d154bc063cab59ebd8807f7a169350f26
Kierran McPherson <kierran.mcpherson@xero.com>
2015-03-12 16:53:54 +1300

    Changed format of substitute username and password

--- Assets/config.sh
+++ Assets/config.sh
@@ -7,10 +7,10 @@ fi
 
 if [[ -n ${USERNAME} ]]; then
   echo "The username is: ${USERNAME}"
-  sed -i -e "s#\(.*\"{{RPC-USERNAME}}\":\).*#\1 \"${USERNAME}\",#g" $CONFIG/settings.json
+  sed -i -e "s#\(.*\"rpc-username\":\).*#\1 \"${USERNAME}\",#g" $CONFIG/settings.json
 fi
 
 if [[ -n ${PASSWORD} ]]; then
   echo "The password is: ${PASSWORD}"
-  sed -i -e "s#\(.*\"{{RPC-PASSWORD}}\":\).*#\1 \"${PASSWORD}\",#g" $CONFIG/settings.json
+  sed -i -e "s#\(.*\"rpc-password\":\).*#\1 \"${PASSWORD}\",#g" $CONFIG/settings.json
 fi
