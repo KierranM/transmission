c5260afd6cef1db2e1a0b211b91d178e6e60be5d
Kierran McPherson <kierranm@gmail.com>
2015-03-21 23:38:58 +1300

    Moved rpc whitelist from being a parameter in transmission.sh to setting the value in settings.json

--- Assets/transmission.sh
+++ Assets/transmission.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 umask 000
 
-exec /sbin/setuser root transmission-daemon --foreground --allowed 127.*,192.168.* --log-info
+exec /sbin/setuser root transmission-daemon --foreground --log-info
